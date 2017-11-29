0375d7b3b015a415ef1a17cdfb3be60e0c475025
Eradicos <hugottfreakz@web.de>
2017-11-29 17:31:52 +0100

    Ai verbessert und Spawn verbessert

--- Galaxy Invaders/Assets/TestStuff/Scripts/Networking/Spawn.cs
+++ Galaxy Invaders/Assets/TestStuff/Scripts/Networking/Spawn.cs
@@ -6,12 +6,23 @@ using UnityEngine.Networking;
 public class Spawn : NetworkBehaviour {
 
     public GameObject spawn;
+    public float spawntime;
+    public float lifeTime;
+    private int counter;
     // Use this for initialization
     
 
     public override void OnStartServer()
     {
-        GameObject obj = (GameObject)Instantiate(spawn, new Vector3(14f, 3f, 105f), transform.rotation);
+   
+            InvokeRepeating("SpawnEnemy", spawntime, spawntime);
+      
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject obj = (GameObject)Instantiate(spawn, transform.position, transform.rotation);
         NetworkServer.Spawn(obj);
+        Destroy(obj, lifeTime);
     }
 }
