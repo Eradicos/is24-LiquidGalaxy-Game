7e13f19cea87a803c13b502292444db68b71f0f7
Eradicos <hugottfreakz@web.de>
2017-12-06 16:54:08 +0100

    Ai verbessert und destroy Area erstellt

---
+++ Galaxy Invaders/Assets/TestStuff/Scripts/DestroyEnemys.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemys : MonoBehaviour {
+
+    public new GameObject DestroyObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "")
+        {
+            Destroy(gameObject, .5f);
+        }
+        Destroy(DestroyObject);
+    }
+}
